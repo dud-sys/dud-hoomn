9b31a4ef6984e4de3713d2e1caffce66cfaf1627
Omri Bernstein <hippiccolo@gmail.com>
2023-01-27 19:28:57 -0500

    Try building with pnpm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <ul>
-  <li><a href='/attempt-01'>Attempt 1</a></li>
-  <li><a href='/attempt-02'>Attempt 2</a></li>
+  <li><a href='{base}/attempt-01'>Attempt 1</a></li>
+  <li><a href='{base}/attempt-02'>Attempt 2</a></li>
 </ul>
